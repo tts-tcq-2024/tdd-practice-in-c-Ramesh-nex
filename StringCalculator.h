8e61a88183cb166e0c47f0c49f3d131e8d601421
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-08 11:02:43 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -4,6 +4,12 @@ int isnotemptystr(const char *input)
     }
    return 0;
 }
+int isnotsingledigit(const char *input)
+{
+        if((input[1]=='\0')&&(input[0] >= '0' && input[0] <= '9')){
+        return 1; 
+    }
+   return 0;
 
 int add(const char *input) {
    if(isnotemptystr(input))
