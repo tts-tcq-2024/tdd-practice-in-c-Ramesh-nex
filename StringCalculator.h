5c81b472a2f0ca628bbb2ee19e1d412b7dc2600d
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-08 10:49:54 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,6 +1,14 @@
+boolean isemptystr(const char *input)
+{ if (input == NULL || input[0] == '\0') {
+        return TRUE; 
+    }
+   return FALSE;
+}
+
 int add(const char *input) {
-   if (input == NULL || input[0] == '\0') {
-        return 0; 
+   if(isemptystr(input))
+   {
+  return 0;
     }
-   return 1;
-    }
+ return -1;
+}
