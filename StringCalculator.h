204fb9a5c781b98c85b7fcfb0a200dc125b4248c
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-08 11:57:49 +0530

    done

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,9 +1,9 @@
 int isempty(const char *input)
 {
    if (input == NULL || input[0] == '\0') {
-        return 0; 
+        return 1; 
     }
-           return 1; 
+           return 0; 
 }
 int issingledigit(const char *input)
 {
