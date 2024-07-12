16192ac645dc35fd6b516acb9f69d833fbef99dc
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-12 17:55:16 +0530

    3

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,52 @@
+int add_arr(int *arr,int cnt)
+{
+    int i = 0;
+    int total = 0;
+    while(i < cnt)
+    {
+        if(arr[i] < 1000)
+        {
+          total += arr[i];
+        }
+        i++;
+    }
 
+    return total;
+}
+void strtonum(const char* ch, int *num_arr,int *cnt)
+{
+    static int num = 0;
+
+    if(check_num(ch[*cnt]))
+    {
+       num = num * 10 + ch[*cnt] - '0';
+       if(check_num(ch[(*cnt)+1]))
+       {
+           (*cnt)++;
+       }
+       else
+       {
+          num_arr[*cnt] = num;
+          num = 0;
+          (*cnt)++;
+       }
+    }
+    else
+    {
+        (*cnt)++;
+        num = 0;
+    }
+}
 int sum(const char *input)
 {
    int idx =0;
    int total=0;
+   int arr[100] = {0};
    while(input[idx] != '\0')
       {
-         total += input[idx];
+         strtonum(input,arr,&idx);
       }
+    total = add_arr(arr,idx);
   return total; 
 }
 int isempty(const char *input)
