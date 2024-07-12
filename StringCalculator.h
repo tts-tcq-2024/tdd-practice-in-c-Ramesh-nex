1e65a87b58091494fc2f7d881a2a5a16d7922b18
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-12 19:57:32 +0530

    fin

--- StringCalculator.h
+++ StringCalculator.h
@@ -1,12 +1,3 @@
-int issingledigit(const char input)
-{
-   if(input>=0&&input<=9 )
-   {
-      return 1;
-   }
-   return 0;
-}
-
 int sum(const char *input)
 {
    char *token;
@@ -44,7 +35,7 @@ int issinglechar(const char *input)
    int ret;
    if(strlen(input)==1)
    {
-      ret = issingledigit(input[0]);
+      ret = input[0];
    }
    else
    {
