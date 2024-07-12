e971e19ae36355f8d72200a34eb164623020fbb4
Ramesh-nex <rameshsaravanan16091996@gmail.com>
2024-07-12 18:25:56 +0530

    Update StringCalculator.h

--- StringCalculator.h
+++ StringCalculator.h
@@ -6,56 +6,17 @@ int issingledigit(const char input)
    }
    return 0;
 }
-int add_arr(int *arr,int cnt)
-{
-    int i = 0;
-    int total = 0;
-    while(i < cnt)
-    {
-        if(arr[i] < 1000)
-        {
-          total += arr[i];
-        }
-        i++;
-    }
-
-    return total;
-}
-void strtonum(const char* ch, int *num_arr,int *cnt)
-{
-    static int num = 0;
-
-    if(issingledigit(ch[*cnt]))
-    {
-       num = num * 10 + ch[*cnt] - '0';
-       if(issingledigit(ch[(*cnt)+1]))
-       {
-           (*cnt)++;
-       }
-       else
-       {
-          num_arr[*cnt] = num;
-          num = 0;
-          (*cnt)++;
-       }
-    }
-    else
-    {
-        (*cnt)++;
-        num = 0;
-    }
-}
 int sum(const char *input)
 {
-   
-  return 3; 
+   return 3; 
 }
 int isempty(const char *input)
 {
-   if (input == NULL || input[0] == '\0') {
+   if (strlen(input)==0)
+   {
         return 1; 
-    }
-           return 0; 
+   }
+        return 0; 
 }
 
 int issinglechar(const char *input)
